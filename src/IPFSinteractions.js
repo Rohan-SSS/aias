5a0ffd102256aa058e16f0fdcf304855b1d88713
RohanSSS <singhrohan2533@gmail.com>
2024-02-01 19:02:11 +0530

    minor changes

--- src/IPFSinteractions.js
+++ src/IPFSinteractions.js
@@ -1,19 +1,19 @@
 import * as IPFS from 'ipfs-http-client';
 import { TextEncoder } from 'text-encoding';
 
-const { web3, testFormContract } = require('./ethereumSetup')
+const { web3, testIPFSContract } = require('./ethereumSetup')
 const ipfs = IPFS.create({ host: '127.0.0.1', port: 5001, protocol: 'http' });
 
 
 // add id, cid to blockchain
 async function addFormDataToBlockchain(ipfsCID, id) {
   const accounts = await web3.eth.getAccounts()
-  await testFormContract.methods.addFormData(ipfsCID, id).send({ from: accounts[0], gas: 600000 })
+  await testIPFSContract.methods.addFormData(ipfsCID, id).send({ from: accounts[0], gas: 600000 })
 }
 
 // retrieve cid from blockchain
 async function retrieveCIDFromBlockchain(id) {
-  const result = await testFormContract.methods.getFormData(id).call();
+  const result = await testIPFSContract.methods.getFormData(id).call();
   return result
 }
 
