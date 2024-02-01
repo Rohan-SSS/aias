88b4cbe3180ea89873acb18cb0ae942507251380
RohanSSS <singhrohan2533@gmail.com>
2024-02-01 09:58:58 +0530

    modified ABI

--- src/ethereumSetup.js
+++ src/ethereumSetup.js
@@ -2,23 +2,22 @@ import Web3 from 'web3';
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
-let testFormContractAddress = '0x9DafB8f094B5c86500dFbdeAD405c993eB833e80';
-
+let testFormContractAddress = '0xe08e4409B167739486b0e76c500F3Ec90D689905';
 let testFormContractABI = [
   {
     "anonymous": false,
     "inputs": [
       {
         "indexed": false,
-        "internalType": "uint256",
-        "name": "id",
-        "type": "uint256"
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
       },
       {
         "indexed": false,
-        "internalType": "string",
-        "name": "ipfsCID",
-        "type": "string"
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
       }
     ],
     "name": "FormSubmitted",
@@ -41,14 +40,14 @@ let testFormContractABI = [
   {
     "inputs": [
       {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
         "internalType": "uint256",
         "name": "_id",
         "type": "uint256"
-      },
-      {
-        "internalType": "string",
-        "name": "_ipfsCID",
-        "type": "string"
       }
     ],
     "name": "addFormData",
@@ -78,10 +77,88 @@ let testFormContractABI = [
   }
 ];
 
+let testIPFSContractAddress = '0x018Dd12cFa4f69B1C3e238A3C51aE2F93119ebeE';
+let testIPFSContractABI = [
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "ipfsCID",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      }
+    ],
+    "name": "FormSubmitted",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "idCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_ipfsCID",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
+      }
+    ],
+    "name": "addFormData",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
+      }
+    ],
+    "name": "getFormData",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  }
+]
+
 const testFormContract = new web3.eth.Contract(testFormContractABI, testFormContractAddress);
+const testIPFSContract = new web3.eth.Contract(testIPFSContractABI, testIPFSContractAddress);
+
 
 export {
   testFormContract,
+  testIPFSContract,
   web3
 };
 
